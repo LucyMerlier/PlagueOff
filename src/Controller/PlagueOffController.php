34d816a8386d6d067e532b4094eade8d256c4d18
axelhamil <axel.hamil11@gmail.com>
2020-11-20 14:56:43 +0100

    fix complexity

--- src/Controller/PlagueOffController.php
+++ src/Controller/PlagueOffController.php
@@ -24,11 +24,11 @@ class PlagueOffController extends AbstractController
 
         // Check if POST is OK
         if (!empty($_POST['userName']) && !empty($_POST['asshole'])) {
+            $userName = ucfirst(trim($_POST['userName']));
+            $assholeName = ucfirst(trim($_POST['asshole']));
+
             if (!empty($_POST['hardFuckOff'])) {
                 if ($_POST['hardFuckOff'] === 'Tell them to fuck right off') {
-                    $userName = ucfirst(trim($_POST['userName']));
-                    $assholeName = ucfirst(trim($_POST['asshole']));
-
                     // links to API results
                     $apiUrls = [
                         'https://www.foaas.com/back/' . $assholeName . '/' . $userName,
@@ -47,26 +47,22 @@ class PlagueOffController extends AbstractController
                 }
             } elseif (!empty($_POST['mediumFuckOff'])) {
                 if ($_POST['mediumFuckOff'] === 'Tell a joke, then tell them to back off') {
-                    $userName = ucfirst(trim($_POST['userName']));
-
                     // Link to API result
                     $apiUrl = 'https://official-joke-api.appspot.com/random_joke';
 
                     // Not checking if they return API_ERROR because meh
                     $content = PlagueOffManager::getJoke($apiUrl) .
-                    " Now that we had a good laugh, back off. Best regards, " . $userName;
+                    " Now that we had a good laugh, back off, " . $assholeName . ". Best regards, " . $userName;
                     $voiceMessage = PlagueOffManager::getVoiceRssSound(urlencode($content));
                 }
             } elseif (!empty($_POST['lightFuckOff'])) {
                 if ($_POST['lightFuckOff'] === 'Gently ask them to back away') {
-                    $userName = ucfirst(trim($_POST['userName']));
-
                     // Link to API result
                     $apiUrl = 'https://complimentr.com/api';
 
                     // Not checking if they return API_ERROR because meh
                     $content = PlagueOffManager::getCompliment($apiUrl) .
-                    ", but please, don't stay too close. Thank you, " . $userName;
+                    ", but please, " . $assholeName . ", don't stay too close. Thank you, " . $userName;
                     $voiceMessage = PlagueOffManager::getVoiceRssSound(urlencode($content));
                 }
             }
